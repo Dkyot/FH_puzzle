5ddcf01c361367cb268126ae1548363b9df5fe7e
KonovalovNikolai <konovv.nikk@gmail.com>
2023-07-29 12:08:24 +0300

    Main menu start

--- Assets/Scripts/NewUI/MainMenu/MainMenuController.cs
+++ Assets/Scripts/NewUI/MainMenu/MainMenuController.cs
@@ -6,19 +6,16 @@ public sealed class MainMenuController : VisualElement {
     public new class UxmlFactory : UxmlFactory<MainMenuController, UxmlTraits> { }
 
     public event Action PlayClicked;
+    public event Action SettingsClicked;
 
     private Button _playButton;
     private Button _settingsButton;
-
-    private VisualElement _settings;
 
     public void Init() {
         var _buttonsContainer = this.Q<VisualElement>("ButtonContainer");
 
         _playButton = _buttonsContainer.Q<VisualElement>("PlayButton").Query<Button>("Button");
         _settingsButton = _buttonsContainer.Q<VisualElement>("SettingsButton").Query<Button>("Button");
-
-        _settings = this.Q<VisualElement>("Settings");
 
         _playButton.clicked += OnPlayClicked;
         _settingsButton.clicked += OnSettingsClicked;
@@ -29,7 +26,6 @@ public sealed class MainMenuController : VisualElement {
     }
 
     private void OnSettingsClicked() {
-        Debug.Log("OnSettingsClicked");
-        _settings.visible = !_settings.visible;
+        PlayClicked?.Invoke();
     }
 }

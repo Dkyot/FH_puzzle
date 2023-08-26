d538ff16cc3946f5ec1bbaa07cdd1f8141d0ae5b
KonovalovNikolai <64379069+KonovalovNikolai@users.noreply.github.com>
2023-08-26 10:42:07 +0300

    No photos label

--- Assets/Scripts/NewUI/Views/Gallery/GalleryView.cs
+++ Assets/Scripts/NewUI/Views/Gallery/GalleryView.cs
@@ -6,10 +6,16 @@ using UnityEngine.UIElements;
 namespace FH.UI.Views.Gallery {
     public class GalleryView : ViewBase {
         private Button _backButton;
+        private Label _noPhotoLabel;
         private VisualElement _photoContainer;
+        private VisualElement _photoScrollView;
+
+        public void SetPhotos() { }
 
         protected override void OnInit() {
             _backButton = this.Q<Button>("BackButton");
+            _noPhotoLabel = this.Q<Label>("NoPhotoLabel");
+            _photoScrollView = this.Q<VisualElement>("PhotoScrollView");
             _photoContainer = this.Q<VisualElement>("PhotoContainer");
         }
 

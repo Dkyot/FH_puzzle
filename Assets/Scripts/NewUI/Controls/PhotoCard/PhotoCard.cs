2ba234ba10e330aaebc8925eb2be1f4a6fcd5d4d
KonovalovNikolai <64379069+KonovalovNikolai@users.noreply.github.com>
2023-11-01 10:00:56 +0300

    remove photo capture text

--- Assets/Scripts/NewUI/Controls/PhotoCard/PhotoCard.cs
+++ Assets/Scripts/NewUI/Controls/PhotoCard/PhotoCard.cs
@@ -5,7 +5,7 @@ namespace FH.UI {
     public sealed class PhotoCard : VisualElement {
         public const string photoCardClass = "photo-card";
         public const string photoCardImageClass = "photo-card-image";
-        public const string hiddenPhotoCardLabelClass = "photo-card-hidden-label";
+        //public const string hiddenPhotoCardLabelClass = "photo-card-hidden-label";
         public const string photoCardLabelContainerClass = "photo-card-label-container";
 
         public const string photoCardTextClass = "photo-card-text";
@@ -14,28 +14,28 @@ namespace FH.UI {
         public const string photoCardAnimationClass = "photo-card-animation";
         public const string photoCardTransitionClass = "photo-card-transition";
 
-        public string Text {
-            get => _text;
-            set {
-                _text = value;
-                SetText();
-            }
-        }
+        //public string Text {
+        //    get => _text;
+        //    set {
+        //        _text = value;
+        //        SetText();
+        //    }
+        //}
 
-        public bool IsHidden {
-            get => _isHidden;
-            set {
-                _isHidden = value;
-                SetHidden();
-            }
-        }
+        //public bool IsHidden {
+        //    get => _isHidden;
+        //    set {
+        //        _isHidden = value;
+        //        SetHidden();
+        //    }
+        //}
 
-        private string _text;
+        //private string _text;
         private bool _isHidden;
 
         private Label _heartLabel;
-        private Label _hiddenPhotoLabel;
-        private LocalizedLabel _photoLabel;
+        //private Label _hiddenPhotoLabel;
+        //private LocalizedLabel _photoLabel;
 
         private VisualElement _photoImage;
 
@@ -46,20 +46,20 @@ namespace FH.UI {
             _photoImage.AddToClassList(photoCardImageClass);
             Add(_photoImage);
 
-            _hiddenPhotoLabel = new Label {
-                text = "?"
-            };
-            _hiddenPhotoLabel.AddToClassList(hiddenPhotoCardLabelClass);
-            _photoImage.Add(_hiddenPhotoLabel);
+            //_hiddenPhotoLabel = new Label {
+            //    text = "?"
+            //};
+            //_hiddenPhotoLabel.AddToClassList(hiddenPhotoCardLabelClass);
+            //_photoImage.Add(_hiddenPhotoLabel);
 
             var labelContainer = new VisualElement();
             labelContainer.AddToClassList(photoCardLabelContainerClass);
             Add(labelContainer);
 
-            _photoLabel = new LocalizedLabel();
-            _photoLabel.ClearClassList();
-            _photoLabel.AddToClassList(photoCardTextClass);
-            labelContainer.Add(_photoLabel);
+            //_photoLabel = new LocalizedLabel();
+            //_photoLabel.ClearClassList();
+            //_photoLabel.AddToClassList(photoCardTextClass);
+            //labelContainer.Add(_photoLabel);
 
             _heartLabel = new Label {
                 text = "<3"
@@ -69,8 +69,8 @@ namespace FH.UI {
             _heartLabel.AddToClassList(photoCardHeart);
             labelContainer.Add(_heartLabel);
 
-            SetText();
-            SetHidden();
+            //SetText();
+            //SetHidden();
         }
 
         public void SetImage(Sprite sprite) =>
@@ -89,30 +89,30 @@ namespace FH.UI {
             AddToClassList(photoCardTransitionClass);
         }
 
-        private void SetText() {
-            if (_isHidden)
-                return;
-            _photoLabel.Label = _text;
-        }
+        //private void SetText() {
+        //    if (_isHidden)
+        //        return;
+        //    _photoLabel.Label = _text;
+        //}
 
-        private void SetHidden() {
-            if (_isHidden) {
-                _heartLabel.style.display = DisplayStyle.None;
-                _hiddenPhotoLabel.style.display = DisplayStyle.Flex;
-                _photoLabel.IsLocalizable = false;
-                _photoLabel.Label = "???";
-            }
-            else {
-                _heartLabel.style.display = DisplayStyle.Flex;
-                _hiddenPhotoLabel.style.display = DisplayStyle.None;
-                _photoLabel.IsLocalizable = true;
-                _photoLabel.Label = _text;
-            }
-        }
+        //private void SetHidden() {
+        //    if (_isHidden) {
+        //        _heartLabel.style.display = DisplayStyle.None;
+        //        _hiddenPhotoLabel.style.display = DisplayStyle.Flex;
+        //        _photoLabel.IsLocalizable = false;
+        //        _photoLabel.Label = "???";
+        //    }
+        //    else {
+        //        _heartLabel.style.display = DisplayStyle.Flex;
+        //        _hiddenPhotoLabel.style.display = DisplayStyle.None;
+        //        _photoLabel.IsLocalizable = true;
+        //        _photoLabel.Label = _text;
+        //    }
+        //}
 
         public new sealed class UxmlTraits : VisualElement.UxmlTraits {
-            private UxmlStringAttributeDescription _labelValue = new() { name = "Text", defaultValue = "Sex" };
-            private UxmlBoolAttributeDescription _isHiddenValue = new UxmlBoolAttributeDescription { name = "IsHidden", defaultValue = false };
+            //private UxmlStringAttributeDescription _labelValue = new() { name = "Text", defaultValue = "Sex" };
+            //private UxmlBoolAttributeDescription _isHiddenValue = new UxmlBoolAttributeDescription { name = "IsHidden", defaultValue = false };
 
             public UxmlTraits() { }
 
@@ -121,11 +121,11 @@ namespace FH.UI {
 
                 var ate = ve as PhotoCard;
 
-                var text = _labelValue.GetValueFromBag(bag, cc);
-                ate.Text = text;
+                //var text = _labelValue.GetValueFromBag(bag, cc);
+                //ate.Text = text;
 
-                bool isHidden = _isHiddenValue.GetValueFromBag(bag, cc);
-                ate.IsHidden = isHidden;
+                //bool isHidden = _isHiddenValue.GetValueFromBag(bag, cc);
+                //ate.IsHidden = isHidden;
             }
         }
 

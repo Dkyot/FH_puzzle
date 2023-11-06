afac4efc79d415723698b06467dbd19dd361dd3f
KonovalovNikolai <64379069+KonovalovNikolai@users.noreply.github.com>
2023-11-06 11:59:36 +0300

    Level settings

--- Assets/Scripts/SOData/LevelDataSO.cs
+++ Assets/Scripts/SOData/LevelDataSO.cs
@@ -19,8 +19,8 @@ namespace FH.SO {
         [Serializable]
         public sealed class LevelParams {
             [field: SerializeField] public ColorsSO Palete { get; private set; }
-            [field: SerializeField, Range(2, 6)] public int Rows { get; private set; }
-            [field: SerializeField, Range(2, 6)] public int Columns { get; private set; }
+            [field: SerializeField, Range(2, 8)] public int Rows { get; private set; }
+            [field: SerializeField, Range(2, 8)] public int Columns { get; private set; }
             [field: SerializeField] public bool UseTwoPair { get; private set; }
         }
     }

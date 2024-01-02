ee119328f45e04452106c7ec15e0a1f7f6345f07
KonovalovNikolai <64379069+KonovalovNikolai@users.noreply.github.com>
2024-01-02 18:39:12 +0300

    UI improvements

---
+++ Assets/Scripts/NewUI/Controls/LoadingDots.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace FH.UI {
+    [UxmlElement]
+    public sealed partial class LoadingDots : Label {
+        private const string _dotsFirstStep = ".";
+        private const string _dotsSecodStep = "..";
+        private const string _dotsThirdStep = "...";
+        private int _currentStep = 0;
+
+        private CancellationTokenSource _tokenSource;
+
+        public LoadingDots() {
+            text = _dotsFirstStep;
+
+            if (Application.isPlaying) {
+                RegisterCallback<AttachToPanelEvent>((e) => StartAnimation());
+                RegisterCallback<DetachFromPanelEvent>((e) => CancelAnimation());
+            }
+        }
+
+        public void StartAnimation() {
+            if (_tokenSource != null)
+                return;
+            _tokenSource = new CancellationTokenSource();
+            _ = Animate(_tokenSource.Token);
+        }
+
+        public void CancelAnimation() {
+            _tokenSource?.Cancel();
+            _tokenSource = null;
+        }
+
+
+        private async Awaitable Animate(CancellationToken cancellationToken) {
+            while (!cancellationToken.IsCancellationRequested) {
+                Debug.Log("Tick");
+
+                await Awaitable.WaitForSecondsAsync(0.5f, cancellationToken);
+
+                _currentStep++;
+                var stepIndex = _currentStep % 3;
+
+                text = stepIndex switch {
+                    0 => _dotsFirstStep,
+                    1 => _dotsSecodStep,
+                    2 => _dotsThirdStep,
+                    _ => _dotsFirstStep,
+                };
+            }
+        }
+    }
+}
--- Assets/Scripts/NewUI/Views/Loading/LoadingView.cs
+++ Assets/Scripts/NewUI/Views/Loading/LoadingView.cs
@@ -5,8 +5,9 @@ using UnityEngine.UIElements;
 
 namespace FH.UI.Views.Loading {
     public class LoadingView : ViewBase {
-        protected override void OnInit() {
-        }
+        private LoadingDots _dots;
+
+        protected override void OnInit() { }
 
         public new sealed class UxmlFactory : UxmlFactory<LoadingView, UxmlTraits> { }
     }

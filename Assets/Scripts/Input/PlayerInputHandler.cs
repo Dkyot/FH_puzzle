25134228b83cc3ff8ac2f5cc17d3a8f944c5c8f8
KonovalovNikolai <64379069+KonovalovNikolai@users.noreply.github.com>
2024-01-11 20:14:40 +0300

    Change input module

--- Assets/Scripts/Input/PlayerInputHandler.cs
+++ Assets/Scripts/Input/PlayerInputHandler.cs
@@ -1,31 +1,16 @@
 using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace FH.Inputs {
     public class PlayerInputHandler : MonoBehaviour {
         public event Action<Vector2> Pressed;
 
-        public Vector2 MousePosition => _input.InGame.Position.ReadValue<Vector2>();
+        public Vector2 MousePosition => Input.mousePosition;
 
-        private Controls _input;
-
-        private void Awake() {
-            _input = new Controls();
-        }
-
-        private void OnClick(InputAction.CallbackContext context) {
-            Pressed?.Invoke(_input.InGame.Position.ReadValue<Vector2>());
-        }
-
-        private void OnEnable() {
-            _input.Enable();
-            _input.InGame.LMB_click.performed += OnClick;
-        }
-
-        private void OnDisable() {
-            _input.Disable();
-            _input.InGame.LMB_click.performed -= OnClick;
+        private void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                Pressed?.Invoke(MousePosition);
+            }
         }
     }
 }

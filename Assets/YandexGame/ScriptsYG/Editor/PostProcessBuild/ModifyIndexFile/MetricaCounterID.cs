6b5ed5358caea0b6acbbc88f7f3ec313cdded9fd
Sergei Shubenkov <sergunwe152@gmail.com>
2024-01-17 14:50:04 +0300

    Added event call on metric initialization

--- Assets/YandexGame/ScriptsYG/Editor/PostProcessBuild/ModifyIndexFile/MetricaCounterID.cs
+++ Assets/YandexGame/ScriptsYG/Editor/PostProcessBuild/ModifyIndexFile/MetricaCounterID.cs
@@ -5,14 +5,25 @@ namespace YG.EditorScr.BuildModify
 {
     public partial class ModifyBuildManager
     {
-        public static void SetMetricaCounterID()
+        public static void SetMetrica()
         {
             if (infoYG.metricaEnable)
             {
-                string donorPatch = Application.dataPath + "/YandexGame/ScriptsYG/Editor/PostProcessBuild/ModifyIndexFile/MetricaDonor.html";
+                string donorPatch = Application.dataPath +
+                                    "/YandexGame/ScriptsYG/Editor/PostProcessBuild/ModifyIndexFile/MetricaDonor.html";
                 string donorText = File.ReadAllText(donorPatch);
-                donorText = donorText.Replace("METRICA_COUNTER_ID", infoYG.metricaCounterID.ToString());
+                if (infoYG.metricaSendWhenInit)
+                {
+                    donorText = donorText.Replace("METRICA_INIT_EVENT_NAME", infoYG.metricaInitEventName);
+                }
+                else
+                {
+                    donorText = donorText.Replace(
+                        "ym(METRICA_COUNTER_ID, 'reachGoal', 'METRICA_INIT_EVENT_NAME'); //Send init event",
+                        "//Send init event disable");
+                }
 
+                donorText = donorText.Replace("METRICA_COUNTER_ID", infoYG.metricaCounterID.ToString());
                 AddIndexCode(donorText, CodeType.head);
             }
         }
--- Assets/YandexGame/ScriptsYG/InfoYG/InfoYG.cs
+++ Assets/YandexGame/ScriptsYG/InfoYG/InfoYG.cs
@@ -411,6 +411,10 @@ namespace YG
         [ConditionallyVisible(nameof(metricaEnable))]
         [Min(0)]
         public int metricaCounterID;
+        [ConditionallyVisible(nameof(metricaEnable))]
+        public bool metricaSendWhenInit;
+        [ConditionallyVisible(nameof(metricaSendWhenInit))]
+        public string metricaInitEventName;
 
         [Header("———————  Other settings  ———————")]
 

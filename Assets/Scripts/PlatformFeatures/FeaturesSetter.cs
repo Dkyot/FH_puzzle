02b4daa32af8780a9dbc553f9332625724e7abb7
Sergei Shubenkov <sergunwe152@gmail.com>
2024-01-21 00:02:17 +0300

    Optimize and wegl template

--- Assets/Scripts/PlatformFeatures/FeaturesSetter.cs
+++ Assets/Scripts/PlatformFeatures/FeaturesSetter.cs
@@ -6,21 +6,16 @@ namespace PlatformFeatures
 {
     public class FeaturesSetter : MonoBehaviour
     {
-        [SerializeField] private bool debug;
         [SerializeField] private List<GameObject> editorFeatures;
         [SerializeField] private List<GameObject> buildFeatures;
 
         private void Awake()
         {
-#if UNITY_EDITOR
+#if UNITY_EDITOR || DEBUG
             var currentFeatures = editorFeatures;
 #else
             var currentFeatures = buildFeatures;
 #endif
-            if (debug)
-            {
-                currentFeatures = editorFeatures;
-            }
             foreach (var featureObject in currentFeatures)
             {
                 Instantiate(featureObject);

f8bdfda6c678f94e7f9c62173d0e83524f0b9e55
Sergei Shubenkov <cergei.we@mail.ru>
2025-01-12 21:58:58 +0300

    Change save module

--- Assets/Scripts/Platforms/Features/Save/YandexGamesSaveFeature.cs
+++ Assets/Scripts/Platforms/Features/Save/YandexGamesSaveFeature.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using Newtonsoft.Json;
+using Platforms.Save;
 using UnityEngine;
 using YG;
+using YG.Insides;
 
 namespace Platforms.Save
 {
     public class YandexGamesSaveFeature : ISaveFeature
     {
-        public SaveInfo SaveInfo { get; private set; } = new ();
+        public SaveInfo SaveInfo => YG2.saves.saveInfo;
         
         public event Action DataLoadedEvent;
 
@@ -16,68 +18,75 @@ namespace Platforms.Save
 
         public YandexGamesSaveFeature()
         {
-            // if (YandexGame.SDKEnabled)
-            // {
-            //     LoadData();
-            // }
+            if (YG2.isSDKEnabled)
+            {
+                LoadData();
+            }
         }
         
         public void InitCallbacks()
         {
             if(_callbackInit) return;
-            
-            //YandexGame.GetDataEvent += DataLoadedEvent;
+
+            YG2.onGetSDKData += DataLoadedEvent;
             DataLoadedEvent += LoadData;
             _callbackInit = true;
         }
         
         ~YandexGamesSaveFeature()
         {
-            //YandexGame.GetDataEvent -= DataLoadedEvent;
+            YG2.onGetSDKData -= DataLoadedEvent;
             DataLoadedEvent -= LoadData;
         }
 
         public void LoadData()
         {
-            // if (_dataLoaded) return;
-            // SaveInfo = YandexGame.savesData.saveInfo;
-            // if (string.IsNullOrEmpty(SaveInfo.Language))
-            // {
-            //     YandexGame.LanguageRequest();
-            //     SaveInfo.Language = YandexGame.EnvironmentData.language;
-            // }
-            //
-            // Debug.Log(JsonConvert.SerializeObject(SaveInfo));
-            //
-            // _dataLoaded = true;
-            // DataLoadedEvent?.Invoke();
+            if (_dataLoaded) return;
+            YGInsides.LoadProgress();
+            if (string.IsNullOrEmpty(SaveInfo.Language))
+            {
+                YG2.GetEnvirData();
+                SaveInfo.Language = YG2.envir.language;
+            }
+            
+            Debug.Log(JsonConvert.SerializeObject(SaveInfo));
+            
+            _dataLoaded = true;
+            DataLoadedEvent?.Invoke();
         }
 
         public void SaveData()
         {
-            // SaveInfo.LastSaveTimeTicks = DateTime.UtcNow.Ticks;
-            // YandexGame.savesData.saveInfo = SaveInfo;
-            // YandexGame.SaveProgress();
+            SaveInfo.LastSaveTimeTicks = DateTime.UtcNow.Ticks;
+            YG2.SaveProgress();
         }
 
 #if UNITY_2023_1_OR_NEWER
         public async Awaitable<bool> LoadDataAwaitable(uint waitingTimeSeconds)
         {
-            // if (YandexGame.SDKEnabled)
-            // {
-            //     LoadData();
-            //     return true;
-            // }
-            //
-            // var time = DateTime.UtcNow;
-            // var timeout = TimeSpan.FromSeconds(waitingTimeSeconds);
-            // while (!_dataLoaded && DateTime.UtcNow - time < timeout)
-            // {
-            //     await Awaitable.NextFrameAsync();
-            // }
+            if (YG2.isSDKEnabled)
+            {
+                LoadData();
+                return true;
+            }
+            
+            var time = DateTime.UtcNow;
+            var timeout = TimeSpan.FromSeconds(waitingTimeSeconds);
+            while (!_dataLoaded && DateTime.UtcNow - time < timeout)
+            {
+                await Awaitable.NextFrameAsync();
+            }
 
             return _dataLoaded;
         }
 #endif
+    }
+}
+
+namespace YG
+{
+    public partial class SavesYG
+    {
+        public SaveInfo saveInfo = new();
     }
 }

a5e58172d094ed9e5b95b9401e5f36ea3136af79
Sergei Shubenkov <cergei.we@mail.ru>
2025-01-14 00:43:38 +0300

    Added infinite music

---
+++ Assets/Scripts/Sound/InfiniteMusic.cs
@@ -0,0 +1,23 @@
+﻿using FH.SO;
+using UnityEngine;
+
+namespace FH.Sound
+{
+    public sealed class InfiniteMusic : MonoBehaviour
+    {
+        [SerializeField] private SettingsSO settings;
+        [SerializeField] private AudioSource audioSource;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+            settings.MusicVolumeChanged += OnMusicVolumeChanged;
+            OnMusicVolumeChanged();
+        }
+
+        private void OnMusicVolumeChanged()
+        {
+            audioSource.volume = settings.MusicVolume;
+        }
+    }
+}
